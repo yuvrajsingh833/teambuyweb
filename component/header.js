e208bb451a0b276b9936f555314f3a628af719c2
sachin.meena <sachin.meena@csiworld.in>
2022-11-10 18:26:27 +0530

    Header updated

--- component/header.js
+++ component/header.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function header({ }) {
     return (
         <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
 
             <title>Teambuy</title>
 
